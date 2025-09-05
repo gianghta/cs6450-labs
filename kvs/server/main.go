c8bc89385687583b47283b56c1968e99c72844d1
Giang Ta <thoanggiang98@gmail.com>
2025-09-04 19:51:13 -0600

    Revert "remove mutex with atomic counter"
    This reverts commit 9480f8ce8f7c66c9375db84959ca25afe7ee986b.

--- kvs/server/main.go
+++ kvs/server/main.go
@@ -4,47 +4,45 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"math/rand/v2"
 	"net"
 	"net/http"
 	"net/rpc"
 	"sync"
-	"sync/atomic"
 	"time"
 
 	"github.com/rstutsman/cs6450-labs/kvs"
 )
 
-const numCounterShards = 256
-
 type Stats struct {
-	puts []atomic.Uint64
-	gets []atomic.Uint64
-}
-
-type PrevStats struct {
+	sync.Mutex
 	puts uint64
 	gets uint64
+}
+
+func (s *Stats) Sub(prev *Stats) Stats {
+	r := Stats{}
+	r.puts = s.puts - prev.puts
+	r.gets = s.gets - prev.gets
+	return r
 }
 
 type KVService struct {
 	mp        sync.Map
 	stats     Stats
-	prevStats PrevStats
+	prevStats Stats
 	lastPrint time.Time
 }
 
 func NewKVService() *KVService {
 	kvs := &KVService{}
-	kvs.stats.puts = make([]atomic.Uint64, numCounterShards)
-	kvs.stats.gets = make([]atomic.Uint64, numCounterShards)
 	kvs.lastPrint = time.Now()
 	return kvs
 }
 
 func (kv *KVService) Get(request *kvs.GetRequest, response *kvs.GetResponse) error {
-	shardIndex := rand.Uint32N(numCounterShards)
-	kv.stats.gets[shardIndex].Add(1)
+	kv.stats.Lock()
+	kv.stats.gets++
+	kv.stats.Unlock()
 
 	value, ok := kv.mp.Load(request.Key)
 	if ok {
@@ -54,53 +52,41 @@ func (kv *KVService) Get(request *kvs.GetRequest, response *kvs.GetResponse) err
 }
 
 func (kv *KVService) Put(request *kvs.PutRequest, response *kvs.PutResponse) error {
-	shardIndex := rand.Uint32N(numCounterShards)
-	kv.stats.puts[shardIndex].Add(1)
+	kv.stats.Lock()
+	kv.stats.puts++
+	kv.stats.Unlock()
 	kv.mp.Store(request.Key, request.Value)
 
 	return nil
 }
 
 func (kv *KVService) BatchGet(request *kvs.BatchGetRequest, response *kvs.BatchGetResponse) error {
-	numGets := len(request.Requests)
-
-	response.Responses = make([]kvs.GetResponse, numGets)
-	for i, req := range request.Requests {
-		if value, ok := kv.mp.Load(req.Key); ok {
-			response.Responses[i].Value = value.(string)
+	response.Responses = make([]kvs.GetResponse, len(request.Requests))
+	for i, request := range request.Requests {
+		if err := kv.Get(&request, &response.Responses[i]); err != nil {
+			return err
 		}
 	}
-
-	if numGets > 0 {
-		shardIndex := rand.Uint32N(numCounterShards)
-		kv.stats.gets[shardIndex].Add(uint64(numGets))
-	}
-
 	return nil
 }
 
 func (kv *KVService) printStats() {
-	var currentPuts uint64
-	var currentGets uint64
-	for i := 0; i < numCounterShards; i++ {
-		currentGets += kv.stats.gets[i].Load()
-		currentPuts += kv.stats.puts[i].Load()
-	}
-
+	kv.stats.Lock()
+	stats := kv.stats
+	prevStats := kv.prevStats
+	kv.prevStats = stats
 	now := time.Now()
-	deltaS := now.Sub(kv.lastPrint).Seconds()
-
-	diffGets := currentGets - kv.prevStats.gets
-	diffPuts := currentPuts - kv.prevStats.puts
-
-	kv.prevStats.gets = currentGets
-	kv.prevStats.puts = currentPuts
+	lastPrint := kv.lastPrint
 	kv.lastPrint = now
+	kv.stats.Unlock()
 
-	fmt.Printf("get/s %.2f\nput/s %.2f\nops/s %.2f\n\n",
-		float64(diffGets)/deltaS,
-		float64(diffPuts)/deltaS,
-		float64(diffGets+diffPuts)/deltaS)
+	diff := stats.Sub(&prevStats)
+	deltaS := now.Sub(lastPrint).Seconds()
+
+	fmt.Printf("get/s %0.2f\nput/s %0.2f\nops/s %0.2f\n\n",
+		float64(diff.gets)/deltaS,
+		float64(diff.puts)/deltaS,
+		float64(diff.gets+diff.puts)/deltaS)
 }
 
 func main() {
